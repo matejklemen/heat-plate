64de8ff808bc3a2c5444044c79f325b8d2f1167b
Rok Grmek <grmekrok@gmail.com>
2017-11-15 13:22:46 +0100

    First working version with pthreads.

--- src/heat_plate_pt.c
+++ src/heat_plate_pt.c
@@ -7,47 +7,41 @@
 
 #define NUM_THREADS 4
 
-struct thread_params
+typedef struct
 {
 	int start_row;
 	int end_row;
-	// max sprememba temperature, ki jo je nasla posamezna nit
-	double curr_max_diff;
-};
+	double max_diff;
+}
+thread_data_t;
 
-// niti in parametri
-pthread_t workers[NUM_THREADS];
-struct thread_params *worker_params[NUM_THREADS];
+static double **first_plate, **second_plate;
+static int global_height, global_width;
 
-// lastnosti plosce (globalne, da poenostavimo zadevo)
-double **first_plate, **second_plate;
-int global_height, global_width;
-
-static void* calc_rows_pt(void *arg)
+static void *calc_rows_pt(void *arg)
 {
-	struct thread_params* params = (thread_params *)arg;
+	thread_data_t *data_ptr = (thread_data_t *)arg;
+	
+	data_ptr->max_diff = 0.0;
 
-	for(int i = params->start_row; i < params->end_row; i++) {
+	for(int i = data_ptr->start_row; i < data_ptr->end_row; i++)
+	{
 		for(int j = 1; j < global_width - 1; j++)
 		{
 			first_plate[i][j] = calc_heat_point(second_plate, i, j);
+
 			double curr_diff = fabs(first_plate[i][j] - second_plate[i][j]);
 
-			// vsaka nit si zapise lokalni maksimum spremembe temperature
-			if(curr_diff > params->curr_max_diff)
-				params->curr_max_diff = curr_diff;
+			if(curr_diff > data_ptr->max_diff)
+				data_ptr->max_diff = curr_diff;
 		}
 	}
+
+	return NULL;
 }
 
 double **calc_heat_plate_pt(int height, int width, int iterations)
 {
-	global_height = height;
-	global_width = width;
-
-	int start_height = 1;
-	int end_height = height - 1;
-
 	/*
 		first_plate... plosca, za katero bomo v tej iteraciji racunali nove vrednosti
 		second_plate... plosca, iz katere bomo vzemali vrednosti za izracun novih vrednosti plosce 'first_plate'
@@ -55,23 +49,23 @@ double **calc_heat_plate_pt(int height, int width, int iterations)
 	first_plate = alloc_plate(height, width);
 	second_plate = alloc_plate(height, width);
 	
+	global_height = height;
+	global_width = width;
+	
 	init_plate(first_plate, height, width);
 	init_plate(second_plate, height, width);
 
-	double max_diff = 0;
+	pthread_t thread[NUM_THREADS];
+	thread_data_t thread_data[NUM_THREADS];
 
-	// TODO: razdelitev dela
-	// Kako razdeliti preostanek (ki se ne da razdeliti povsem pravicno) - sproti (dodaten parameter strukture)
-	// ali na koncu (cakanje na niti, ponovno poganjanje niti)?
+	// enkratno racunanje mej za deljenje dela med niti
 	for(int i = 0; i < NUM_THREADS; i++)
 	{
-		struct thread_params *p = (struct thread_params *) malloc(sizeof(struct thread_params));
-		p->start_row = i + 1;
-		p->end_row = i + 2;
-		p->curr_max_diff = 0;
-
-		worker_params[i] = p;
+		thread_data[i].start_row = 1 + (int)((double)i * (height - 2) / NUM_THREADS);
+		thread_data[i].end_row = 1 + (int)((double)(i + 1) * (height - 2) / NUM_THREADS);
 	}
+
+	double max_diff;
 
 	for(int k = 0; k < iterations; k++)
 	{
@@ -79,26 +73,21 @@ double **calc_heat_plate_pt(int height, int width, int iterations)
 
 		// inicializacija niti in racunanje
 		for(int i = 0; i < NUM_THREADS; i++)
-			pthread_create(&workers[i], NULL, calc_rows_pt, (void *)worker_params[i]);
+			pthread_create(&thread[i], NULL, calc_rows_pt, (void *)&thread_data[i]);
 
 		// cakanje na niti
 		for(int i = 0; i < NUM_THREADS; i++)
 		{
-			pthread_join(workers[i], NULL);
+			pthread_join(thread[i], NULL);
+			
 			// iskanje maksimuma delnih rezultatov niti
-			if(worker_params[i]->curr_max_diff > max_diff)
-				max_diff = worker_params[i]->curr_max_diff;
-
-			worker_params[i]->curr_max_diff = 0;
+			if(thread_data[i].max_diff > max_diff)
+				max_diff = thread_data[i].max_diff;
 		}
 		
 		swap_pointers(&first_plate, &second_plate);
 	}
 
-	// pocistimo strukture za prenasanje parametrov niti
-	for(int i = 0; i < NUM_THREADS; i++)
-		free(worker_params[i]);
-	
 	// ne rabimo vec prve plosce
 	free_plate(first_plate, height, width);
 	
@@ -109,4 +98,3 @@ double **calc_heat_plate_pt(int height, int width, int iterations)
 	
 	return second_plate;
 }
-
--- src/main.c
+++ src/main.c
@@ -32,15 +32,15 @@ int main(int argc, char *argv[])
 	{
 		double **solution_plate = calc_heat_plate_pt(height, width, iterations);
 		
-		// IplImage *img = get_image(solution_plate, height, width);
+		IplImage *img = get_image(solution_plate, height, width);
 		
-		// show_image(img);
+		show_image(img);
 		
-		// char file_name[64];
-		// sprintf(file_name, "img/heat_plate_%s_%s_%s.png", argv[1], argv[2], argv[3]);
-		// save_image(img, file_name);
+		char file_name[64];
+		sprintf(file_name, "img/heat_plate_%s_%s_%s.png", argv[1], argv[2], argv[3]);
+		save_image(img, file_name);
 		
-		// release_image(img);
+		release_image(img);
 		
 		free_plate(solution_plate, height, width);
 	}
