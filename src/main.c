41fb02320fa6d5f8168c2d40a375b14b0b486cef
Rok Grmek <grmekrok@gmail.com>
2017-12-10 18:57:12 +0100

    Updated included files in 'main.c'.

--- src/main.c
+++ src/main.c
@@ -2,8 +2,9 @@
 #include "heat_plate.h"
 #include "visualization.h"
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
