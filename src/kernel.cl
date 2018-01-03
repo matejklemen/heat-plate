9ce506e7ed23d140b361de8392e7b63384cc66b6
Rok Grmek <grmekrok@gmail.com>
2018-01-03 21:11:23 +0100

    OpenCL algorithm update.

--- src/kernel.cl
+++ src/kernel.cl
@@ -1,27 +1,26 @@
-__kernel void calculate_point(__global float *curr_state,
-                              __global float *prev_state,
-                              __global float *temp_max_diff,
+__kernel void calculate_point(__global float *first_plate,
+                              __global float *second_plate,
+                              __global int *all_difs_below_eps,
                               int height,
-                              int width)
+                              int width,
+                              float eps)
 {
-	int id = get_local_id(0);
-	int size = get_local_size(0);
-	
-	temp_max_diff[id] = 0.0f;
+	int id = get_global_id(0);
+	int size = get_global_size(0);
 	
 	for(int cell_idx = width + id; cell_idx < ((height - 1) * width); cell_idx += size)
 	{
 		if((cell_idx + 1) % width > 1)
 		{
-			curr_state[cell_idx] = (prev_state[cell_idx - width] +
-			                        prev_state[cell_idx - 1] +
-			                        prev_state[cell_idx + 1] +
-			                        prev_state[cell_idx + width]) / 4;
+			first_plate[cell_idx] = (second_plate[cell_idx - width] +
+			                         second_plate[cell_idx - 1] +
+			                         second_plate[cell_idx + 1] +
+			                         second_plate[cell_idx + width]) / 4;
 			
-			float curr_diff = curr_state[cell_idx] - prev_state[cell_idx];
+			float curr_diff = fabs(first_plate[cell_idx] - second_plate[cell_idx]);
 			
-			if(curr_diff > temp_max_diff[id])
-				temp_max_diff[id] = curr_diff;
+			if(curr_diff > eps)
+				*all_difs_below_eps = 0;
 		}
 	}
 }
--- src/opencl_algorithm.c
+++ src/opencl_algorithm.c
@@ -13,189 +13,107 @@
 
 float **calc_heat_plate_opencl(int height, int width, float epsilon)
 {
-	cl_int ret;
-	
 	// branje datoteke
-	FILE *fp;
-	char *source_str;
-	size_t source_size;
-	
-	fp = fopen("src/kernel.cl", "r");
-	if (!fp) 
+	FILE *fp = fopen("src/kernel.cl", "r");
+	if(!fp)
 	{
 		fprintf(stderr, "Error reading the file\n");
 		exit(1);
 	}
-	source_str = (char *) malloc(MAX_SOURCE_SIZE);
-	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+	char *source_str = (char *) malloc(MAX_SOURCE_SIZE);
+	size_t source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
 	source_str[source_size] = '\0';
 	fclose(fp);
 	
-	float **plate = alloc_plate(height, width);
-	init_plate(plate, height, width);
-	
-	/*
-		Za shranjevanje delnih max temperaturnih razlik. S tem je implementirana
-		redukcija -> namesto, da bi sli vedno na koncu iteracije cez cel array
-		(height * width operacij), naredi graficna delne rezultate - max
-		razlike za posamezno nit, mi pa na hostu najdemo max izmed teh delnih
-		rezultatov (MAX_WORKGROUP_SIZE operacij).
-	*/
-	float *temp_max_diff = (float *) malloc(MAX_WORKGROUP_SIZE * sizeof(float));
-	
-	// podatki o platformi
+	// podatki o platformi in napravah
 	cl_platform_id platform_id[10];
 	cl_uint ret_num_platforms;
-	
-	ret = clGetPlatformIDs(10, platform_id, &ret_num_platforms);
-	
-	// podatki o napravi
+	clGetPlatformIDs(10, platform_id, &ret_num_platforms);
 	cl_device_id device_id[10];
 	cl_uint ret_num_devices;
+	clGetDeviceIDs(platform_id[0], CL_DEVICE_TYPE_GPU, 10, device_id, &ret_num_devices);
 	
-	ret = clGetDeviceIDs(platform_id[0], CL_DEVICE_TYPE_GPU, 10,	
-	                     device_id, &ret_num_devices);
-	
-	//printf("[Getting device IDs] %s\n", getErrorString(ret));
-	
-	/*
-		kontekst - okolje za izvajanje scepca, upravljanje s pomnilnikom,...
-		(NULL = privzeti kontekst)
-	*/
-	cl_context context = clCreateContext(NULL, 1, &device_id[0], NULL, NULL, &ret);
-	
-	//printf("[Creating context]: %s\n", getErrorString(ret));
- 	
-	// ukazna vrsta
-	cl_command_queue command_queue = clCreateCommandQueue(context, device_id[0], 0, &ret);											
-	
-	//printf("[Creating command queue]: %s\n", getErrorString(ret));
-	
-	// obseg dela, ki ga bo delala ena skupina
-	size_t local_item_size = MAX_WORKGROUP_SIZE;
-	
-	size_t num_groups = (height * width - 1) / local_item_size + 1;
-	size_t global_item_size = num_groups * local_item_size;
-	
-	//printf("Number of groups: %lu, size of group: %lu\n", num_groups, local_item_size);
+	// kontekst, ukazna vrsta, program, prevajanje, scepec
+	cl_context context = clCreateContext(NULL, 1, &device_id[0], NULL, NULL, NULL);
+	cl_command_queue command_queue = clCreateCommandQueue(context, device_id[0], 0, NULL);
+	cl_program program = clCreateProgramWithSource(context,	1, (const char **)&source_str, NULL, NULL);
+	clBuildProgram(program, 1, &device_id[0], NULL, NULL, NULL);
+	cl_kernel kernel = clCreateKernel(program, "calculate_point", NULL);
 	
 	/*
-		Alokacija pomnilnika na graficni kartici.
-		Oznaceno z "g_", ker se podatki nahajajo na graficni kartici.
-	*/	
-	cl_mem g_first_plate = clCreateBuffer(context,
-	                                      CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
-	                                      height * width * sizeof(float), plate[0], &ret);
-	
-	//printf("[Allocating (g_) first plate]: %s\n", getErrorString(ret));
-	
-	cl_mem g_second_plate = clCreateBuffer(context,
-	                                       CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
-	                                       height * width * sizeof(float), plate[0], &ret);
-	
-	//printf("[Allocating (g_) second plate]: %s\n", getErrorString(ret));
-	
-	cl_mem g_temp_max_diff = clCreateBuffer(context,
-	                                        CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
-	                                        MAX_WORKGROUP_SIZE * sizeof(float), temp_max_diff, &ret);
-	
-	//printf("[Allocating (g_) temp_max_diff]: %s\n", getErrorString(ret));
-	
-	// priprava programa
-	cl_program program = clCreateProgramWithSource(context,	1,
-	                                               (const char **)&source_str, NULL, &ret);
-	
-	//printf("[Preparing program]: %s\n", getErrorString(ret));
-	
-	// prevajanje
-	ret = clBuildProgram(program, 1, &device_id[0], NULL, NULL, NULL);
-	
 	// log
 	size_t build_log_len;
 	char *build_log;
-	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, 
-	                            0, NULL, &build_log_len);
-	
-	build_log =(char *)malloc(sizeof(char)*(build_log_len+1));
-	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, 
-	                            build_log_len, build_log, NULL);
-	//printf("%s\n", build_log);
+	clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, 0, NULL, &build_log_len);
+	build_log = (char *) malloc(sizeof(char) * (build_log_len + 1));
+	clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, build_log_len, build_log, NULL);
+	printf("%s\n", build_log);
 	free(build_log);
+	*/
 	
-	// scepec: priprava objekta
-	cl_kernel kernel = clCreateKernel(program, "calculate_point", &ret);
+	// inicializacija plosce in priprava pomnilnika (za dve plosci) na graficni kartici
+	float **plate = alloc_plate(height, width);
+	init_plate(plate, height, width);
 	
-	size_t buf_size_t;
-	clGetKernelWorkGroupInfo(kernel, device_id[0], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE,sizeof(buf_size_t), &buf_size_t, NULL);
-	//printf("veckratnik niti = %lu\n", buf_size_t);
-	
+	cl_mem g_first_plate = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, height * width * sizeof(float), plate[0], NULL);
 	cl_mem *g_first_plate_ptr = &g_first_plate;
+	
+	cl_mem g_second_plate = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, height * width * sizeof(float), plate[0], NULL);
 	cl_mem *g_second_plate_ptr = &g_second_plate;
+	
+	// inicializacija zastavice za konec iteracij in priprava pomnilnika na graficni kartici
+	int all_difs_below_eps = 1;
+	cl_mem g_all_difs_below_eps = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(int), &all_difs_below_eps, NULL);
+	
+	// obseg dela
+	size_t local_item_size = MAX_WORKGROUP_SIZE;
+	size_t global_item_size = ((height * width - 1) / local_item_size + 1) * local_item_size;
 	
 	int iterations = 0;
 	
-	/*
-		1. Izracunamo novo stanje in delne max temperaturne razlike (na graficni).
-		2. Izracunamo max temperaturno razliko v trenutni iteraciji (na hostu).
-		3. Pogledamo, ce je max temperaturna razlika < epsilon.
-	*/
 	while(1)
 	{
-		iterations++;
-		float curr_max = 0;
+		// nastavimo zacetno vrednost zastavice in jo prenesemo v pomnilnik graficne kartice
+		all_difs_below_eps = 1;
+		clEnqueueWriteBuffer(command_queue, g_all_difs_below_eps, CL_TRUE, 0, sizeof(int), (void *)&all_difs_below_eps, 0, NULL, NULL);
 		
-		// scepec: argumenti
-		ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)g_first_plate_ptr);
-		ret |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)g_second_plate_ptr);
-		ret |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&g_temp_max_diff);
-		ret |= clSetKernelArg(kernel, 3, sizeof(cl_int), (void *)&height);
-		ret |= clSetKernelArg(kernel, 4, sizeof(cl_int), (void *)&width);
+		// nastavimo argumente in pozenemo racunanje ene iteracije na graficni kartici
+		clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)g_first_plate_ptr);
+		clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)g_second_plate_ptr);
+		clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&g_all_difs_below_eps);
+		clSetKernelArg(kernel, 3, sizeof(cl_int), (void *)&height);
+		clSetKernelArg(kernel, 4, sizeof(cl_int), (void *)&width);
+		clSetKernelArg(kernel, 5, sizeof(cl_float), (void *)&epsilon);
 		
-		// scepec: zagon
-		ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
-		                             &global_item_size, &local_item_size, 0, NULL, NULL);
+		clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, NULL);
 		
-		// kopiranje maksimimalnih razlik v temperaturi
-		ret = clEnqueueReadBuffer(command_queue, g_temp_max_diff, CL_TRUE, 0,		
-		                          MAX_WORKGROUP_SIZE * sizeof(float), temp_max_diff, 0, NULL, NULL);	    
-		
-		//printf("[Copying results]: %s\n", getErrorString(ret));
-		
-		// redukcija (max)
-		for(int i = 0; i < MAX_WORKGROUP_SIZE; i++)
-		{
-			if(temp_max_diff[i] > curr_max)
-				curr_max = temp_max_diff[i];
-		}
-		
-		//printf("Current max diff is %.3f\n", curr_max);
-		
-		if(curr_max < epsilon)
-			break;
-		
-		// zamenjamo plosci
+		// zamenjamo plosci in povecamo stevec iteracij
 		cl_mem *tmp = g_first_plate_ptr;
 		g_first_plate_ptr = g_second_plate_ptr;
 		g_second_plate_ptr = tmp;
+		
+		iterations++;
+		
+		// preberemo zastavico iz pomnilnika graficne kartice in po potrebi zakljucimo iteracije
+		clEnqueueReadBuffer(command_queue, g_all_difs_below_eps, CL_TRUE, 0, sizeof(int), (void *)&all_difs_below_eps, 0, NULL, NULL);
+		
+		if(all_difs_below_eps)
+			break;
 	}
 	
-	// kopiranje rezultatov
-	ret = clEnqueueReadBuffer(command_queue, *g_second_plate_ptr, CL_TRUE, 0,		
-	                          height * width * sizeof(float), plate[0], 0, NULL, NULL);
+	// kopiranje rezultatov iz graficne kartice
+	clEnqueueReadBuffer(command_queue, g_second_plate, CL_TRUE, 0, height * width * sizeof(float), plate[0], 0, NULL, NULL);
 	
 	// brisanje podatkov graficne kartice
-	ret = clFlush(command_queue);
-	ret = clFinish(command_queue);
-	ret = clReleaseKernel(kernel);
-	ret = clReleaseProgram(program);
-	ret = clReleaseMemObject(g_first_plate);
-	ret = clReleaseMemObject(g_second_plate);
-	ret = clReleaseMemObject(g_temp_max_diff);
-	ret = clReleaseCommandQueue(command_queue);
-	ret = clReleaseContext(context);
-	
-	// brisanje podatkov na hostu
-	free(temp_max_diff);
+	clFlush(command_queue);
+	clFinish(command_queue);
+	clReleaseKernel(kernel);
+	clReleaseProgram(program);
+	clReleaseMemObject(g_first_plate);
+	clReleaseMemObject(g_second_plate);
+	clReleaseMemObject(g_all_difs_below_eps);
+	clReleaseCommandQueue(command_queue);
+	clReleaseContext(context);
 	
 #ifndef TIME_MEASUREMENTS
 	
